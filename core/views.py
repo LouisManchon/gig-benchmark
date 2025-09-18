e53ba9ef98fb809f2861b46d3653c41f451e798e
LouisManchon <10578@holbertonstudents.com>
2025-09-18 02:48:17 +0200

    integrate ModelViewSet classes with serializers for CRUD endpoints

--- core/views.py
+++ core/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Sport, MarketName, League, Team, Player
+from .serializers import (
+    SportSerializer, MarketNameSerializer, LeagueSerializer, TeamSerializer, PlayerSerializer
+)
 
-# Create your views here.
+class SportViewSet(viewsets.ModelViewSet):
+    queryset = Sport.objects.all()
+    serializer_class = SportSerializer
+
+class MarketNameViewSet(viewsets.ModelViewSet):
+    queryset = MarketName.objects.all()
+    serializer_class = MarketNameSerializer
+
+class LeagueViewSet(viewsets.ModelViewSet):
+    queryset = League.objects.all()
+    serializer_class = LeagueSerializer
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+class PlayerViewSet(viewsets.ModelViewSet):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
